9e7c384666ef4c979d826ff3e7e3b795f865f9d6
Alex <alexridge2309@gmail.com>
2022-11-03 18:44:01 +0000

    Updated code

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,14 +1,16 @@
-a = 1
-while a < 21 
-  if a % 15 == 0
-    puts "fizzbuzz"
-  elsif a % 3 == 0
-    puts "fizz"
-  elsif a % 5 == 0
-    puts "buzz"
-  else 
-    puts a
+def fizzbuzz
+  a = 1
+  while a < 21 
+    if a % 15 == 0
+      puts "fizzbuzz"
+    elsif a % 3 == 0
+      puts "fizz"
+    elsif a % 5 == 0
+      puts "buzz"
+    else 
+      puts a
+    end 
+    a += 1 
   end 
-a += 1 
 end 
-
+fizzbuzz
